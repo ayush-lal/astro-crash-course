4207888fb5bea80289fe947e14eeaccc00e9758d
ayush-lal <ayush.lal@outlook.com>
2023-03-24 12:36:54 +1000

    testing gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/ayush-lal",
-  base: "/astro-crash-course",
+  site: "https://github.com/ayush-lal/astro-crash-course",
 });
